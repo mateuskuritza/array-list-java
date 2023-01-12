090ece89280fe7381a357140e818f6ac97d8546d
mateuskuritza <mateuskuritza@gmail.com>
2023-01-12 18:15:57 -0300

    found duplicates in two lists

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        List<String> firstList = new ArrayList<>();
+
+        firstList.add("Morango");
+        firstList.add("Uva");
+        firstList.add("Acerola");
+        firstList.add("Manga");
+
+        List<String> secondList = new ArrayList<>();
+
+        secondList.add("Pêra");
+        secondList.add("Caju");
+        secondList.add("Morango");
+        secondList.add("Kiwi");
+
         
+        List<String> duplicates = new ArrayList<>();
+        
+        firstList.forEach(element -> {
+            if(secondList.contains(element)){
+                duplicates.add(element);
+            }
+        });
+
+        System.out.println(duplicates);
     }
 }
